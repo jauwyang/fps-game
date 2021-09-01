231937f963271c0e65c04cc9fbc18b1f4e6bd8ea
Joshua Auw-Yang <joshua.auwyang@gmail.com>
2021-09-01 02:26:13 -0400

    updated player movement relative to the player's POV angle

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from raycasting import Player
 from map import Map
 import pygame
 import math
+from math_tools import Vector2D
 
 # ==== GLOBAL VARIABLES ====
 CLOCK = pygame.time.Clock()
@@ -63,15 +64,31 @@ def draw_window(window, game_map, user):
 def keyboard_input(player, map):
     keys = pygame.key.get_pressed()
 
+    # Player movement
+    forward = 0
+    sideways = 0
+    speed_multiplier = 1
+    if keys[pygame.K_LSHIFT]:
+        speed_multiplier *= 2
     if keys[pygame.K_w]:
-        map.wall_collision(player, 0, -5)
+        forward = 2 * speed_multiplier
     if keys[pygame.K_s]:
-        map.wall_collision(player, 0, 5)
+        forward = -2
     if keys[pygame.K_a]:
-        map.wall_collision(player, -5, 0)
+        sideways = 2
     if keys[pygame.K_d]:
-        map.wall_collision(player, 5, 0)
+        sideways = -2
+    sideways_angle = player.heading - math.pi / 2
+    if sideways_angle < 0:
+        sideways_angle += 2 * math.pi
 
+    x_change = forward * math.cos(player.heading)
+    y_change = forward * math.sin(player.heading)
+    x_change += sideways * math.cos(sideways_angle)
+    y_change += sideways * math.sin(sideways_angle)
+    map.wall_collision(player, x_change, y_change)
+
+    # Player rotation
     if keys[pygame.K_LEFT]:
         player.rotate(-0.05)
     if keys[pygame.K_RIGHT]:
@@ -81,6 +98,7 @@ def keyboard_input(player, map):
 def init():
     pygame.init()
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.key.set_repeat(1, 100)
 
     user = Player(100, 100, RED)
     game_map = Map(map)
